8cdfb58965461cf4761fa2aa6174d6dc3c77c5b0
CarterMilne <60952615+CarterMilne@users.noreply.github.com>
2020-04-09 11:04:19 +1200

    Created my existing movement Script

--- Game/Assets/MovementScript.cs
+++ Game/Assets/MovementScript.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public int speed = 7;
+    private Rigidbody2D rb;
+    private float inputHorizontal;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        Run();
+        Jump();
     }
+
+    private void Run()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 12);
+
+        }
+    }
+
 }
+
