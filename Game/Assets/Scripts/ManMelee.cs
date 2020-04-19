7c247b50521f24f638dd41a55e9d864d589cb31d
CarterMilne <60952615+CarterMilne@users.noreply.github.com>
2020-04-20 10:23:17 +1200

    MadeScripts

---
+++ Game/Assets/Scripts/ManMelee.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManMelee : MonoBehaviour
+{
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //play attack animation
+        //detect eminines in range of attack
+        //damage to the enemies
+    }
+}
