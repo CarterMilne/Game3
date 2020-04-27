3b51c0cfb104585dec21be101f28b3e1c855ed50
CarterMilne <60952615+CarterMilne@users.noreply.github.com>
2020-04-27 17:43:27 +1200

    Player Shooting

---
+++ Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+    public float speed = 20f;
+    public float damage = 40f;
+    public Rigidbody2D iRigidbody;
+
+    void Start()
+    {
+        iRigidbody.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+       Enemy enemy = hitInfo.GetComponent<Enemy>();
+            if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
+
--- Game/Assets/Scripts/MovementScript.cs
+++ Game/Assets/Scripts/MovementScript.cs
@@ -8,45 +8,56 @@ public class MovementScript : MonoBehaviour
     [SerializeField] public int speed = 4;
     private Rigidbody2D rb;
     private float inputHorizontal;
-
     public float maxHealth = 100f;
     public float currentHealth;
     public PlayerHealth playerHealth;
-
+    private bool facingLeft = true;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
-        playerHealth = transform.Find("Canvas").Find("PlayerHealth").GetComponent<PlayerHealth>();
+        Canvas[] canvases = FindObjectsOfType<Canvas>();
+        Canvas canvasMain = null;
+        foreach(Canvas canvas in canvases)
+        {
+            if(canvas.gameObject.name == "CanvasHealthBar")
+            {
+                canvasMain = canvas;
+                break;
+            }
+        }
+        playerHealth = canvasMain.transform.Find("Health bar").GetComponent<PlayerHealth>();
+        //playerHealth = transform.Find("Canvas").Find("PlayerHealth").GetComponent<PlayerHealth>();
         playerHealth.SetHealth(maxHealth);
-        
-
+        facingLeft = true;
     }
-
 
     void Update()
-    {
-        Run();
-        Jump();
-    }
-
-    private void Run()
     {
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
 
-    }
-
-    private void Jump()
-    {
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, 8);
-
         }
+
+        Flip(inputHorizontal);
     }
 
+    private void Flip(float inputHorizontal)
+    {
+        if(inputHorizontal > 0 && !facingLeft || inputHorizontal < 0 && facingLeft )
+        {
+            facingLeft = !facingLeft;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
 }
 
