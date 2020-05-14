fa91c65fa5759283996b4077b062630118b6424c
CarterMilne <60952615+CarterMilne@users.noreply.github.com>
2020-05-14 16:59:40 +1200

    Touret Script

---
+++ Game/Assets/Scripts/Touret.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class Touret : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // while (Bullet > 0)
+         //   Fire = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
