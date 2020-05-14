3739b2df6e801d8b029cf527fad0be89243ca4c0
CarterMilne <60952615+CarterMilne@users.noreply.github.com>
2020-05-14 17:02:03 +1200

    Created boss follow script script

---
+++ Game/Assets/Scripts/Boss.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss : MonoBehaviour
+{
+    public float speed = 2.5f;
+    Transform man;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        man = GameObject.FindGameObjectWithTag("Man").transform;
+        rb = GetComponent<Rigidbody2D>();
+        Follow();
+    }
+
+    public void Follow()
+    {
+        Vector2 target = new Vector2(man.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+ 
+   
+}
