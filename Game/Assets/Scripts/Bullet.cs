e83265ac88ce4edbb4ef0201fd0f028ba4d10451
CarterMilne <60952615+CarterMilne@users.noreply.github.com>
2020-05-14 17:02:43 +1200

    Player shooting

--- Game/Assets/Scripts/Bullet.cs
+++ Game/Assets/Scripts/Bullet.cs
@@ -5,33 +5,40 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     
-    public float speed = 20f;
-    public float damage = 25f;
+    public float speed = 12.5f;
+    // speed variable for the speed of the bullet 
+    public float damage = 20f;
+    // the damage set for the bullet 
     public Rigidbody2D iRigidbody;
+    // the rigidbody giving the bullet physics
     public bool isFlipped;
+    // flipping the bullet boolean 
 
     void Start()
     {
         if (FindObjectOfType<MovementScript>().facingLeft)
+            // this is finding the movement script
         {
             transform.rotation = Quaternion.Euler(0, 0, 180);
             iRigidbody.velocity = new Vector3(-1, 0, 0) * speed;
             Debug.Log("I'm Flipped");
-
+            // flipping thebullet using the quaternions
         }
         else
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
             iRigidbody.velocity = new Vector3(1, 0, 0) * speed;
             Debug.Log("I'm Not");
+            // flipping thebullet using the quaternions
         }
 
-        
+
     }
 
     void OnTriggerEnter2D (Collider2D hitInfo)
     {
        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        // finds the enemy component
             if(enemy != null)
         {
             enemy.TakeDamage(damage);
--- Game/Assets/Scripts/BulletScript.cs
+++ Game/Assets/Scripts/BulletScript.cs
@@ -5,27 +5,37 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     float moveSpeed = 7f;
+    // this is the movement speed of the bullet
     Rigidbody2D iRigidbody;
+    // rigidody
     MovementScript target;
+    // the movement script (the player) is the target of the enemies bullet
     Vector2 moveDirection;
-
+    
 
     void Start()
     {
         iRigidbody = GetComponent<Rigidbody2D>();
+        // the rigdbody geting the componetn from the game
         target = GameObject.FindObjectOfType<MovementScript>();
+        // the target of teh attack is the player but called movement script 
         moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        // the direction of the bullet follows the players current positoion
         iRigidbody.velocity = new Vector2(moveDirection.x, moveDirection.y);
-        Destroy(gameObject, 3f);    
+        // the rigidbody of the bullet
+        Destroy(gameObject, 3f);  
     }
     
     void OnTriggerEnter2D (Collider2D col)
+        // when the bullet collides with the player
     {
         if (col.gameObject.name =="Man")
         {
             Debug.Log("Hit!");
             Destroy(gameObject);
-            target.TakeDamage(10f);
+            // when hit the bullet destroys
+            target.TakeDamage(20f);
+            // damage is done to the character
         }
     }
     
